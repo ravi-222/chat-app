00708458764eb61d7963d510d29399e9de779980
ravi-222 <ravindersinghanand22@gmail.com>
2021-02-10 15:30:29 +0530

    import db in sidebarchat

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -1,6 +1,7 @@
 import { Avatar } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
+import db from "../../config";
 
 function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
